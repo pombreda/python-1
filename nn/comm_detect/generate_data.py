ba25a5b961f70c3332338b697710dac8549366d4
pratik.ac <pratik.ac@gmail.com>
2015-01-26 14:00:44 -0800

    H is generated from a hidden signal
    H[i,:] = A*h
    where h is a fixed random signal and A is a random measurement matrix

--- nn/comm_detect/generate_data.py
+++ nn/comm_detect/generate_data.py
@@ -7,13 +7,13 @@ from model import *
 from learner import *
 
 def generate_nn_data(n, l, d, rho, N):
-    H = generate_Y(n, 0, d, rho, N)
+    H = generate_sparse_Y(n, 0, d, rho, N)
     G = signed_nn(n, l, d)
     Y = decoder(G, H)
     return G, H, Y
 
 def generate_nn_test_data(n, l, d, rho, G, N):
-    H = generate_Y(n, 0, d, rho, N)
+    H = generate_sparse_Y(n, 0, d, rho, N)
     Y = decoder(G, H)
     return None, H, Y
 
@@ -34,3 +34,18 @@ def estimate_rho(l, d, Y):
     N, n = Y.shape
     rhoy = np.sum(Y)/float(n*N)
     return rhoy*(2./float(d))**l
+
+# static variables inside the function!
+def generate_sparse_Y(n,l,d,rho,N):
+    if not (hasattr(generate_sparse_Y, "signal") or \
+            hasattr(generate_sparse_Y, "m") or \
+            hasattr(generate_sparse_Y, "density")):
+        m = 10*n
+        signal = np.random.random((m,1))
+        density = 0.2
+    Y = []
+    for i in xrange(N):
+        A = sps.rand(n, m, density)
+        y = np.array(A.dot(signal)).T
+        Y.append(y)
+    return np.array(Y)
